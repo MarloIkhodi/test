5380ada8d5d84a683d7edf7797e887d6916fbec6
MarloIkhodi <deivid.serafim03@gmail.com>
2024-08-05 22:33:30 -0300

    add calc.py to ."

---
+++ calc.py
@@ -0,0 +1,44 @@
+# teste de calculadora simples em python
+def calculadora():
+    As = str(input('''digite (numero) (sinal) (numero)
++ para adição
+- para subtração
+/ para divisão
+* para multiplicação
+'''))
+    B = As.split()
+    C = float(B[0])
+    D = (B[1])
+    E = float(B[2])
+    if D == '+':
+        print('\n{} + {} ='.format(C, E))
+        print(C+E)
+    elif D == '-':
+        print('\n{} - {} ='.format(C, E))
+        print(C-E)
+    elif D == '*':
+        print('\n{} * {} ='.format(C, E))
+        print(C*E)
+    elif D == '/':
+        print('\n{} / {} ='.format(C, E))
+        print(C/E)
+    else:
+        print('operação invalida')
+    repetir()
+
+
+def repetir():
+    denovo = input('''
+Deseja realizar outro calculo?
+S para sim, N para não
+    ''')
+    if denovo.upper() == 'S':
+        calculadora()
+    elif denovo.upper() == 'N':
+        print('Até mais!')
+    else:
+        repetir()
+# test
+
+
+calculadora()
